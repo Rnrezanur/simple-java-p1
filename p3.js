298cca6b406508333a8cd16c210187229c139c5e
Rezanur <rezanurbinshamim@gmail.com>
2026-02-23 05:03:47 +0600

    3rd one

---
+++ p3.js
@@ -0,0 +1,10 @@
+// You task is to divide the given number by 5 and show the remainder as the output.
+// Input:
+// The first line of the input contains the number.
+
+// Output:
+// Print the remainder.
+
+var number = parseInt("119");
+var remainder = number % 5;
+console.log(remainder.toFixed(2));
